92514ad3750016bdce56e1f61b4e70a550ea36b0
ViniciusLisboa07 <viniciuslisboa1001@gmail.com>
2023-03-04 20:34:15 -0300

    feat: product fields and validations

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -46,6 +46,6 @@ class ProductsController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def product_params
-      params.fetch(:product, {})
+      params.fetch(:product, {}).permit(:name, :description, :price)
     end
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,3 @@
 class Product < ApplicationRecord
+  validates :name, :description, :price, presence: { message: "can't be blank" }
 end
